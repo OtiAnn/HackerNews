1785113380de161cb2aa6b718c4025934c5415c4
Annie Chen <anniebh30@gmail.com>
2019-02-17 02:04:01 -0500

    add infinity scroll pagination

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,8 +1,14 @@
 class StoriesController < ApplicationController
   def index
     Story.get_top_stories
-    all_stories = Story.full_content.order(publish_at: :desc)
+    all_stories = Story.full_content.order(publish_at: :desc).first(30)
     @first_story = all_stories.first
-    @story_lists = all_stories.drop(1)
+    @story_lists = all_stories
+    @total_pages = Story.full_content.page(1).total_pages
+  end
+
+  def get_story_list
+    @story_lists = Story.full_content.order(publish_at: :desc).page(params[:page]).per(30)
+    render json: @story_lists
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'stories#index'
+  get 'stories/get_story_list'
 end
